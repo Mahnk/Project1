0734f9943f1b5045e4b5e526344afbfa1a0c4477
Alexey Yushkov <mahnk@yandex.ru>
2013-05-26 02:08:21 +0400

    Непонятно почему китайские иероглифы :(

---
+++ index.php
@@ -0,0 +1,73 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+
+<html xmlns="http://www.w3.org/1999/xhtml">
+
+<head>
+
+<meta http-equiv="Content-Type" content="text/html; charset=windows-1251" />
+
+<title>Росвоенипотека 2.0</title>
+
+<link rel="stylesheet" href="main.css">
+
+<link rel="shortcut icon" href="favicon.ico" type="image/x-icon">
+<link href="Img/top2.jpg.css" rel="stylesheet" type="text/css" />
+</head>
+
+
+
+<body>
+<div id="main_container">
+    	<div id="header_top">
+    		<div id="header_top_logo"></div>
+        	
+	   
+  <!-- Навигация -->
+			<div id="navigation">
+    		<ul>
+        		<li><a href="/index.php">Главная</a></li>
+            	<li><a href="index2.php">Участнику НИС</a></li>
+            	<li><a href="#">Вопрос-Ответ</a></li>
+            	<li><a href="#">Программы</a></li>
+                <li><a href="#">Учреждение</a></li>
+            	<li><a href="#">Контактная информация</a></li>
+        	</ul>
+			</div>
+		</div>
+		<div id="content"> 
+			<div id="left_column"> <p> Филиалы: </p>
+            	<div id="map"> <img src="Img/map.gif"/></div>
+			</div>
+			<div id="center_column"> <img src="Img/gazprom.png"></div>
+			<div id="right_column">
+            	<p>Личный кабинет</p>
+	 		<form action="#">
+    			<input type="text" size="20" id="login" value=" E-mail" 		onFocus='if(this.value==" E-mail")this.value="";' onBlur='if(this.value=="")this.value=" E-mail";'/>
+        		<input type="password" size="20" id="pass" value="Пароль" onFocus='if(this.value=="Пароль")this.value="";' onBlur='if(this.value=="")this.value="Пароль";'/>
+       			<input type="submit" value="Войти" size="40" id="enter"> 
+    		</form>
+            </div>
+		</div>
+        <div id="left_down_column"></div>
+        <div id="center_down_column">
+        	<div id="c_top"><ul>
+            	<li><a href="#"><img src="Img/Icon 1.png" /></a></li>
+                <li><a href="index3.php"><img src="Img/Icon 2.png" /></a></li>
+                <li><a href="#"><img src="Img/Icon 3.png" /></a></li>
+                <li><a href="#"><img src="Img/Icon 4.png" /></a></li>
+                <li><a href="#"><img src="Img/Icon 5.png" /></a></li>
+            </ul>
+            </div>
+            <div id="c_down"></div>
+        </div>
+        <div id="right_down_column">
+        	<div id="r_top"></div>
+            <div id="r_down"></div>
+        </div>
+        <div id="footer"></div>
+        
+</div>
+</body>
+
+</html>
+
